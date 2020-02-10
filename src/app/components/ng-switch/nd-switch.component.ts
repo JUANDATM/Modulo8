4eba3adbe0e22d6486ceb5f4e554af46e72e3440
JUANDATM <kira00711@hotmail.com>
2020-02-10 16:36:08 -0600

    Ejercicios Modulo 8

---
+++ src/app/components/ng-switch/nd-switch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nd-switch',
+  templateUrl: './nd-switch.component.html',
+  styles: []
+})
+export class NdSwitchComponent implements OnInit {
+  alerta: string = "warning";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
